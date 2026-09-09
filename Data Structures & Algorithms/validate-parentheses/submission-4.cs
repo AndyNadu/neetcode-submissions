ca54901c29e37a04461ab360aa73748d2cddea68
andy <119100872+AndyNadu@users.noreply.github.com>
2026-09-09 18:26:04 +0300

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        var pairs = new Dictionary<char, char> {
+            { ')', '(' },
+            { ']', '[' },
+            { '}', '{' }
+        };
+
+        foreach (var ch in s) {
+            if (pairs.ContainsKey(ch)) {
+                if (stack.Count is 0 || stack.Pop() != pairs[ch]) {
+                    return false;
+                }
+            }
+            else {
+                stack.Push(ch);
+            }
+        }
+
+        return stack.Count is 0;
+    }
+}
