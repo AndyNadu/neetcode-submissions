f91d53940954c271ace7578ec994f48f82a632d5
andy <119100872+AndyNadu@users.noreply.github.com>
2026-09-09 18:12:36 +0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        var minPrice = 100;
+        var maxProfit = 0;
+
+        foreach (var price in prices) {
+            maxProfit = Math.Max(maxProfit, price - minPrice);
+            minPrice = Math.Min(minPrice, price);
+        }
+
+        return maxProfit;
+    }
+}
