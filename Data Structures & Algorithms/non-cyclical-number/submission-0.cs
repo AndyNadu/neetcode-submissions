d6914c14ef4b9336d8dcb9e4e7fe75cb07efc100
andy <119100872+AndyNadu@users.noreply.github.com>
2026-09-12 17:53:12 +0300

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    private readonly HashSet<int> invalidNumbers;
+
+    public bool IsHappy(int n) {
+        while (true) {
+            var sum = CalculateSum(n);
+            if (sum is 1) {
+                return true;
+            }
+
+            if (invalidNumbers.Contains(sum)) {
+                return false;
+            }
+
+            invalidNumbers.Add(sum);
+            n = sum;
+        }
+    }
+
+    private int CalculateSum(int n) {
+        var sum = 0;
+
+        while (n != 0) {
+            var lastDigit = n % 10;
+            sum += lastDigit * lastDigit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
